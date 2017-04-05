9e31fb66b7f12451d96a02bd2347c13286d5a684
Will <yh@fir.im>
2017-04-05 16:50:25 +0800

    add java install

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# mvn_install Step
+Install dependencies use `mvn install`
+
+### INPUTS
+
+## EXAMPLE 
+
+```yml
+steps:
+  - name: mvn_install
+    enable: true
+    failure: true
+    plugin:
+      name: mvn_install
+      inputs:
+```
---
+++ run.sh
@@ -0,0 +1,13 @@
+# ************************************************************
+#
+# This step will install your project dependencies
+#
+#   Variables used:
+#
+#   Outputs:
+#
+# ************************************************************
+
+cd $FLOW_CURRENT_PROJECT_PATH
+
+flow_cmd "mvn install" --echo --retry --assert
